57f7ed26aecc1704d4476273c34f43a9cd441ff1
matheusluis-dev <matheusluis.dev@proton.me>
2025-02-04 06:27:17 -0300

    Inherit types assembly

---
+++ tests/mocked-assemblies/types/ArchGuard.MockedAssembly.Inherit/Types.cs
@@ -0,0 +1,15 @@
+﻿namespace ArchGuard.MockedAssembly.Inherit;
+
+public class Class1;
+
+public class InheritClass1 : Class1;
+
+public class DoNotInheritClass1;
+
+public interface IInterface1;
+
+public interface IInheritInterface1 : IInterface1;
+
+public interface IDoNotInheritInterface1;
+
+public class ImplementIInterface1 : IInheritInterface1;
